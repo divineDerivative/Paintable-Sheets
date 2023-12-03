150219663087375b8817f870df0c19df93d3cbce
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2023-12-03 12:33:15 -0600

    Fix for colors being darker than expected

--- Source/HarmonyPatches.cs
+++ Source/HarmonyPatches.cs
@@ -1,23 +1,39 @@
 ﻿using RimWorld;
 using HarmonyLib;
 using UnityEngine;
+using Verse;
 
 namespace PaintableSheets
 {
     [HarmonyPatch(typeof(Building_Bed), nameof(Building_Bed.DrawColorTwo), MethodType.Getter)]
     public static class Building_Bed_DrawColorTwo
     {
-        public static bool Prefix(ref Color __result, Building_Bed __instance)
+        public static bool Prefix(ref Color __result, Building_Bed __instance, ref bool __state)
         {
+            if (__instance.def.graphicData.shaderType != ShaderTypeDefOf.CutoutComplex || !__instance.def.building.paintable)
+            {
+                return true;
+            }
             if (__instance.PaintColorDef == null)
             {
+                if (__instance.def.building.paintable)
+                {
+                    __state = true;
+                }
                 return true;
             }
             __result = __instance.PaintColorDef.color;
             return false;
         }
 
-
+        public static void Postfix(ref Color __result, Building_Bed __instance, bool __state)
+        {
+            if (__state)
+            {
+                Color grey = new Color(121f / 255f, 121f / 255f, 121f / 255f);
+                __result = grey * __result;
+            }
+        }
     }
 
     [HarmonyPatch(typeof(Building_Bed), nameof(Building_Bed.DrawColor), MethodType.Getter)]
@@ -25,6 +41,10 @@ namespace PaintableSheets
     {
         public static bool Prefix(ref Color __result, Building_Bed __instance)
         {
+            if (__instance.def.graphicData.shaderType != ShaderTypeDefOf.CutoutComplex || !__instance.def.building.paintable)
+            {
+                return true;
+            }
             if (__instance.def.MadeFromStuff && __instance.Stuff != null)
             {
                 __result = __instance.def.GetColorForStuff(__instance.Stuff);
